24440642171474081a68020ba89468b967f05726
ibrahim@salah <153787198+salbingo@users.noreply.github.com>
2024-07-19 13:32:39 +0200

    Update 6_Word_Cloud.py

--- pages/6_Word_Cloud.py
+++ pages/6_Word_Cloud.py
@@ -55,7 +55,7 @@ def generate_word_cloud(data, year, column, title):
 with tab1:
     year1 = st.selectbox('select the year to chow the most used word in the Titles', options=years, key=1)
     with st.spinner('wait...'):
-        figure1 = generate_word_cloud(df, year1, 'Title', f'WordCloud for Titles in {year2}')
+        figure1 = generate_word_cloud(df, year1, 'Title', f'WordCloud for Titles in {year1}')
         st.pyplot(figure1)
 
 with tab2:
