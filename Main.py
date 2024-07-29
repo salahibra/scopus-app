34aa30e9dac6a9d963a14e1c39c7b62e9e08fc6a
ibrahim@salah <153787198+IbrahimSalah13@users.noreply.github.com>
2024-07-29 16:21:50 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.set_page_config(
     page_title="ScopusApp",
-    page_icon="🌐"
+    page_icon="〽️🔅🔆☣️"
 )
 
 st.title("Welcome to ScopusApp")
-st.sidebar.success("HELLO", icon="👋")
+st.sidebar.success("HELLO")
 st.write("This application gives  some beatifull charts for data visualization")
 st.write("It gives also statistics for the scopus dataset,  in order to extract some informations.")
